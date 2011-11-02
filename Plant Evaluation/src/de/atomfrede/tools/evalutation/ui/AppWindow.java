b10cf45e03d85e9c9eced9505d84448c2c8919de
frederik.hahne <frederik.hahne@a8f18d0d-325d-ef45-60f1-f56c25066719>
2011-11-02 13:22:42 +0000

    Interface libs
    git-svn-id: https://plant-evaluation.googlecode.com/svn/trunk@28 a8f18d0d-325d-ef45-60f1-f56c25066719

---
+++ Plant Evaluation/src/de/atomfrede/tools/evalutation/ui/AppWindow.java
@@ -0,0 +1,100 @@
+/**
+ * 	AppWindow.java is part of Plant Evaluation.
+ *
+ *  Plant Evaluation is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Plant Evaluation is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Plant Evaluation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.atomfrede.tools.evalutation.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class AppWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		setLookAndFeel();
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					AppWindow window = new AppWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	static void setLookAndFeel() {
+		try {
+			// Set System L&F
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException e) {
+			// handle exception
+		} catch (ClassNotFoundException e) {
+			// handle exception
+		} catch (InstantiationException e) {
+			// handle exception
+		} catch (IllegalAccessException e) {
+			// handle exception
+		}
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AppWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+
+		JMenuItem mntmEvaluate = new JMenuItem("Evaluate");
+		mnFile.add(mntmEvaluate);
+
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+
+		JMenu mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
+
+		JMenuItem mntmOptions = new JMenuItem("Options");
+		mnEdit.add(mntmOptions);
+	}
+
+}
