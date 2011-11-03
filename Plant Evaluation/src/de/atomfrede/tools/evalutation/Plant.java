700a27eb228bba15585daf17bf5c6ab8a3c50dde
frederik.hahne <frederik.hahne@a8f18d0d-325d-ef45-60f1-f56c25066719>
2011-11-03 07:57:29 +0000

    Model object for plants
    git-svn-id: https://plant-evaluation.googlecode.com/svn/trunk@34 a8f18d0d-325d-ef45-60f1-f56c25066719

---
+++ Plant Evaluation/src/de/atomfrede/tools/evalutation/Plant.java
@@ -0,0 +1,69 @@
+/**
+ * 	Plant.java is part of Plant Evaluation.
+ *
+ *  Plant Evaluation is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Plant Evaluation is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Plant Evaluation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.atomfrede.tools.evalutation;
+
+import java.util.Date;
+
+public class Plant {
+
+	Date startDate, endDate;
+	int number;
+
+	public Plant() {
+		this(new Date());
+	}
+
+	public Plant(Date startDate) {
+		this(startDate, new Date());
+	}
+
+	public Plant(Date startDate, Date endDate) {
+		this(startDate, endDate, 0);
+	}
+
+	public Plant(Date startDate, Date endDate, int number) {
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.number = number;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+}
