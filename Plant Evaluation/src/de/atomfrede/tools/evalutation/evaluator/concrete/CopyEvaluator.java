05b714a432052bcca13252414ca3146cb0d36cad
frederik.hahne <frederik.hahne@a8f18d0d-325d-ef45-60f1-f56c25066719>
2011-12-06 20:55:03 +0000

    Comments #5
    git-svn-id: https://plant-evaluation.googlecode.com/svn/trunk@162 a8f18d0d-325d-ef45-60f1-f56c25066719

--- Plant Evaluation/src/de/atomfrede/tools/evalutation/evaluator/concrete/CopyEvaluator.java
+++ Plant Evaluation/src/de/atomfrede/tools/evalutation/evaluator/concrete/CopyEvaluator.java
@@ -34,6 +34,14 @@ import de.atomfrede.tools.evalutation.EntryComparator;
 import de.atomfrede.tools.evalutation.evaluator.common.AbstractEvaluator;
 import de.atomfrede.tools.evalutation.helper.PreProcessor;
 
+/**
+ * First evaluator of every evaluation. Just processes all input files and
+ * copies the content (except the header) into one new file and sorts them
+ * ascending by date and time
+ * 
+ * This makes it easier to find changes of the solenoid valves and to process
+ * date and time of each data set.
+ */
 public class CopyEvaluator extends AbstractEvaluator {
 
 	private final Log log = LogFactory.getLog(CopyEvaluator.class);
@@ -58,16 +66,19 @@ public class CopyEvaluator extends AbstractEvaluator {
 		log.info("Copy Evaluator started");
 		CSVWriter writer = null;
 		try {
+			// create one output file
 			outputFile = new File(outputFolder, "allLaserData.csv");
 			outputFile.createNewFile();
 			if (!outputFile.exists())
 				return false;
-
+			// create the corresponding csv writer to write lines into the
+			// output file
 			writer = getCsvWriter(outputFile);
-			// WriteUtils.writeHeader(writer);
 
+			// read all files in the input folder
 			File[] allInputFiles = inputRootFolder.listFiles();
 			log.debug("#Files " + allInputFiles.length);
+
 			String[] header = null;
 			List<String[]> allLines = new ArrayList<String[]>();
 			for (int i = 0; i < allInputFiles.length; i++) {
@@ -78,25 +89,25 @@ public class CopyEvaluator extends AbstractEvaluator {
 					// read all lines and write them to the new file
 					List<String[]> currentLines = readAllLinesInFile(inputFile);
 					if (currentLines.get(0).length == 1) {
+						// if the length of the current line is equal to 1, the
+						// input files are not separated by comma
+						// instead by a fixed width. Preprocess the file so it
+						// is a nice comma separated file
 						PreProcessor.replaceWhiteSpacesWithComma(inputFile);
 						currentLines = readAllLinesInFile(inputFile);
 					}
-
+					// if there is no header use the first line
 					if (header == null)
 						header = currentLines.get(0);
-
+					// then remove it, because we don't need the header of the
+					// other files anymore
 					currentLines.remove(0);
-					// if (i != 0) {
-					// currentLines.remove(0);
-					// } else {
-					// allLines.add(currentLines.get(0));
-					// }
 
 					for (int j = 1; j < currentLines.size(); j++) {
 						String[] currentLine = currentLines.get(j);
 						double solenoidValue = parseDoubleValue(currentLine,
 								Constants.SOLENOID_VALVE_INPUT);
-
+						// TODO check if this is fine for every evaluation!
 						if (solenoidValue == 1.0 || solenoidValue == 4.0
 								|| solenoidValue == 2.0) {
 							allLines.add(currentLine);
@@ -105,10 +116,11 @@ public class CopyEvaluator extends AbstractEvaluator {
 
 				}
 			}
-			// String[] header = allLines.get(0);
-			// allLines.remove(0);
+			// sort all lines
 			Collections.sort(allLines, new EntryComparator());
+			// add the header as first line
 			allLines.add(0, header);
+			// write all lines to output file
 			writer.writeAll(allLines);
 			progressBar.setValue(100);
 
